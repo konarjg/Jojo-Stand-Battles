f1b870ade3072730435c2a99d0dc4b41cfa1254c
KonarJG <konarjustgames@gmail.com>
2021-03-16 13:09:31 +0100

    0.0.0.4

---
+++ Assets/_MAIN/Source/Menu/Menu.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    private GameObject[] MainMenu;
+    private GameObject[] MatchmakingMenu;
+    private GameObject[] SearchingMenu;
+
+    private static Server Server;
+
+    private void OnEnable()
+    {
+        Server.OnSearchTimeUpdatedEvent += UpdateSearchTime;
+    }
+
+    private void OnDisable()
+    {
+        Server.OnSearchTimeUpdatedEvent -= UpdateSearchTime;
+    }
+
+    private void DisplayMenu(string name)
+    {
+        var fields = GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+        for (int i = 0; i < fields.Length; ++i)
+        {
+            var value = (GameObject[])fields[i].GetValue(this);
+
+            if (fields[i].Name == name)
+            {
+                for (int j = 0; j < value.Length; ++j)
+                    value[j].SetActive(true);
+            }  
+            else
+            {
+                for (int j = 0; j < value.Length; ++j)
+                    value[j].SetActive(false);
+            }
+        }
+    }
+
+    public void Init(Server server)
+    {
+        Server = server;
+
+        var main = GameObject.Find("Main").transform;
+        MainMenu = new GameObject[main.childCount];
+
+        for (int i = 0; i < main.childCount; ++i)
+        {
+            MainMenu[i] = main.GetChild(i).gameObject;
+            MainMenu[i].SetActive(false);
+        }
+
+        var matchmaking = GameObject.Find("Matchmaking").transform;
+        MatchmakingMenu = new GameObject[matchmaking.childCount];
+
+        for (int i = 0; i < matchmaking.childCount; ++i)
+        {
+            MatchmakingMenu[i] = matchmaking.GetChild(i).gameObject;
+            MatchmakingMenu[i].SetActive(false);
+        }
+
+        var searching = GameObject.Find("Match Searching").transform;
+        SearchingMenu = new GameObject[searching.childCount];
+
+        for (int i = 0; i < searching.childCount; ++i)
+        {
+            SearchingMenu[i] = searching.GetChild(i).gameObject;
+            SearchingMenu[i].SetActive(false);
+        }
+    }
+
+    public void Enable()
+    {
+        for (int i = 0; i < MainMenu.Length; ++i)
+           MainMenu[i].SetActive(true);
+    }
+
+    #region Interactions
+
+    public void Play()
+    {
+        DisplayMenu("MatchmakingMenu");
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+    public void Back()
+    {
+        DisplayMenu("MainMenu");
+    }
+
+    public void QuickMatch()
+    {
+        Server.QuickMatch();
+        DisplayMenu("SearchingMenu");
+    }
+
+    public void RankedMatch()
+    {
+        Server.RankedMatch();
+    }
+
+    #endregion
+
+    #region Event Listeners
+
+    public void UpdateSearchTime(float time)
+    {
+        var text = SearchingMenu[0].transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time - minutes * 60f);
+
+        string minutesString = "" + (minutes < 10 ? "0" + minutes : "" + minutes);
+        string secondsString = "" + (seconds < 10 ? "0" + seconds : "" + seconds);
+
+        text.text = string.Format("{0}:{1}", minutesString, secondsString);
+    }
+
+    #endregion
+}
--- Assets/_MAIN/Source/Networking/Server.cs
+++ Assets/_MAIN/Source/Networking/Server.cs
@@ -1,46 +1,97 @@
 ﻿using Jojo;
 using Photon;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MonoBehaviour = Photon.MonoBehaviour;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
+using EventHandler = Jojo.EventHandler;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(PhotonView), typeof(EventHandler))]
 public class Server : MonoBehaviour
 {
-    [SerializeField]
-    [Range(2, 4)]
-    private int MaxPlayers = 2;
+    private byte MaxPlayers = 2;
+    private Menu Menu;
+
+    private float FPS = 0f;
+    private int FrameCount = 0;
+    private float DT = 0f;
+    private float UpdateRate = 4f;
+
+    private bool Searching = false;
+    private float SearchTime = 0f;
+
+    public delegate void UpdateSearchTime(float time);
+    public static UpdateSearchTime OnSearchTimeUpdatedEvent;
 
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+        Menu = GetComponent<Menu>();
+        Menu.Init(this);
         PhotonNetwork.ConnectUsingSettings("1.0");
         PhotonNetwork.player.NickName = Random.Range(0, int.MaxValue).ToString();
     }
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 200, 50), PhotonNetwork.connectionStateDetailed.ToString());
+        var display = string.Format("{0}FPS {1}ms", Mathf.RoundToInt(FPS), Mathf.RoundToInt(PhotonNetwork.GetPing()));
+
+        GUI.Label(new Rect(10, 10, 200, 50), display);
+    }
+
+    private void CountFPS()
+    {
+        FrameCount++;
+        DT += Time.deltaTime;
+
+        if (DT > 1.0f / UpdateRate)
+        {
+            FPS = FrameCount / DT;
+            FrameCount = 0;
+            DT -= 1.0f / UpdateRate;
+        }
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
             Players.DebugList();
+
+        CountFPS();
+
+        if (Searching)
+        {
+            SearchTime += Time.deltaTime;
+            OnSearchTimeUpdatedEvent?.Invoke(SearchTime);
+        }
     }
 
     public void OnJoinedLobby()
     {
+        Menu.Enable();
+    }
+
+    public void QuickMatch()
+    {
         var table = new RoomOptions();
-        PhotonNetwork.JoinRandomRoom(table.CustomRoomProperties, 2);
+        PhotonNetwork.JoinRandomRoom(table.CustomRoomProperties, MaxPlayers);
+        Searching = true;
+        SearchTime = 0f;
+    }
+
+    public void RankedMatch()
+    {
+
     }
 
     public void OnPhotonRandomJoinFailed()
     {
         var options = new RoomOptions();
-        options.MaxPlayers = 2;
+        options.MaxPlayers = MaxPlayers;
         PhotonNetwork.CreateRoom("", options, null);
     }
 
